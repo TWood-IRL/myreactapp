228f0d86497f11c1a64203280915d39c72dfd256
T <TWoodhouse@UNUM.com>
2019-02-19 15:55:40 +0000

    Updating styling introducing new components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 
-class Home extends React.Component{
+class Home extends Component{
     render(){
+      const {user} = this.props ; 
+
         const biggerLead = { //assigning css to a variable 
             fontSize: 1.4 + 'em', 
             fontWeight: 200 
@@ -12,7 +14,9 @@ class Home extends React.Component{
 
             <div className="container text-center">
             <div className="row justify-content-center">
+             
               <div className="col-10 col-md-10 col-lg-8 col-xl-7">
+            
                 <div className="display-4 text-primary mt-3 mb-2" 
                 style={
                     {
@@ -27,19 +31,27 @@ class Home extends React.Component{
                   in, and picks random users to award giveaways. It's a
                   good example of a Single Page Application which includes
                   connection to a database and routing. It's a practical
-                  way to learn <a href="https://reactjs.org/">React</a> 
+                  way to learn <a href="https://reactjs.org/">React</a>{' '}
                   with <a href="https://firebase.google.com">Firebase</a>.
                 </p>
-    
-                <a href="/register" className="btn btn-outline-primary mr-2">
-                  Register
-                </a>
-                <a href="/login" className="btn btn-outline-primary mr-2">
-                  Log In
-                </a>
-                <a href="/meetings" className="btn btn-primary"> {/*This is a comment  */}
-                  Meetings
-                </a>
+                    {user == null && (
+                      
+                      <span>
+                        <a href="/register" className="btn btn-outline-primary mr-2">
+                          Register
+                        </a>
+                        <a href="/login" className="btn btn-outline-primary mr-2">
+                          Log In
+                        </a>
+                      </span>
+                    )}
+
+                    {user && (
+                        <a href="/meetings" className="btn btn-primary"> {/*This is a comment  */}
+                        Meetings
+                        </a>
+                    ) }
+               
               </div>{' '}
             </div>
         </div>
