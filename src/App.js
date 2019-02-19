c68acc4b26282e4de58df8c907a38af4023ed812
T <TWoodhouse@UNUM.com>
2019-02-19 11:54:18 +0000

    Cleaning project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
+// Import React
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Shadow Realm
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Meeting Log</h1>
       </div>
     );
   }
