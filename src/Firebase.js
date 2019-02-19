c41d23d18d7c20fb657ebb6ef1b7cc916d470f22
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2019-02-19 19:22:29 +0000

    Updating to use process variables

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database' ;
 import 'firebase/auth' ;  
 
 const config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId
   };
   firebase.initializeApp(config);
 
